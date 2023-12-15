256362128b0c89b5c52a135152628045f98cec23
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-15 12:15:13 +0200

    install mongoose and connect to mongodb

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
-const port = process.env.PORT ?? 3000
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT ?? 3000
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING
 
 const app = new express()
 const router = express.Router()
@@ -11,8 +14,13 @@ router.get('/api', (req, res) => {
     })
 })
 
-app.use(router)
-
-app.listen(port, () => {
-    console.log('api running')
+mongoose.connect(MONGODB_CONNECTION_STRING).then(() => {
+    console.log('connected to mongodb')
+    app.listen(PORT, () => {
+        console.log('api running')
+    })
+}).catch((error) => {
+    console.log(`could not connect to mongodb: ${error}`)
 })
+
+app.use(router)
