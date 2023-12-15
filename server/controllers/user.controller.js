b33da5c7e00d85c23597c48e9f7c9236d86ba894
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-15 15:56:20 +0200

    add login route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 exports.signUp = (req, res) => {
     const email = req.body.email
@@ -24,6 +27,49 @@ exports.signUp = (req, res) => {
     }).catch(error => {
         res.status(500).json({
             message: 'signup failed'
+        })
+    })
+}
+
+exports.logIn = (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({ email: email }).then(user => {
+        if (user == null) {
+            res.status(500).json({
+                message: 'login failed'
+            })
+            return
+        }
+
+        bcrypt.compare(password, user.password).then(valid => {
+            if (!valid) {
+                res.status(401).json({
+                    message: 'authentication failed'
+                })
+                return
+            }
+
+            const token = jwt.sign(
+                {
+                    email: email,
+                    userId: user._id
+                },
+                JWT_SECRET,
+                {
+                    expiresIn: '1h'
+                }
+            )
+
+            res.status(200).json({
+                token: token,
+                userId: user._id
+            })
+        })
+    }).catch(error => {
+        res.status(401).json({
+            message: 'authentication failed'
         })
     })
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/user.controller')
 const router = express.Router()
 
 router.post('/signup', UserController.signUp)
+router.post('/login', UserController.logIn)
 
 module.exports = router
