76f0a741aaa0a696918a0e007e822c2692443264
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-16 10:13:27 +0200

    change CORS policy

--- server/main.js
+++ server/main.js
@@ -11,6 +11,16 @@ const app = new express()
 const router = express.Router()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    next()
+});
+
 app.use('/api', router)
 
 router.use('/user', userRoutes)
