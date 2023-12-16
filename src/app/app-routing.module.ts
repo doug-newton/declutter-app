87260f0cff5e05bfcf4d1179863dfccd93123230
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-16 16:40:13 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AddClutterComponent } from './clutter/add-clutter/add-clutter.component';
+import { authGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
-  { path: 'clutter/add', component: AddClutterComponent }
+  { path: 'clutter/add', component: AddClutterComponent, canActivate: [authGuard] }
 ];
 
 @NgModule({
---
+++ src/app/shared/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { tap } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  const auth = inject(AuthService)
+
+  return auth.isLoggedIn$.pipe(tap(
+    loggedIn => {
+      if (!loggedIn) {
+        router.navigate(['/auth/login'])
+      }
+    }
+  ))
+
+  return true;
+};
