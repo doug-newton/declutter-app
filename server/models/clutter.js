fc1ff5e358cd73e8bd36220aca909c254bc1ee1b
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-17 22:55:13 +0200

    add familyId to clutter and fix addedBy reference

--- server/models/clutter.js
+++ server/models/clutter.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const clutterSchema = mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String },
-    addedBy: { type: mongoose.Schema.Types.ObjectId, required: true }
+    addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    familyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Family', required: true },
 })
 
 module.exports = mongoose.model('Clutter', clutterSchema)
