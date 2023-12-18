3a7b63f3ba0680dfaf1c8b8a9791bd1928283381
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-18 08:39:31 +0200

    add name to user model

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    name: { type: String, required: true },
     family: { type: mongoose.Schema.Types.ObjectId, ref: 'Family' }
 })
 
